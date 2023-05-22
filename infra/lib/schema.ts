5b7e2b5113f16c4559fdf5cb394477dea27ac328
Douglas Naphas <douglasnaphas@gmail.com>
2023-05-22 09:49:00 -0400

    Remove files for non-OIDC assets

--- infra/lib/schema.ts
+++
@@ -1,5 +0,0 @@
-class Schema {
-  static PK = "PK";
-  static SK = "SK";
-}
-module.exports = Schema;
--- rolename/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  roots: ["<rootDir>"],
-  testMatch: ["**/*.test.ts", "**/*.test.js"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-}
