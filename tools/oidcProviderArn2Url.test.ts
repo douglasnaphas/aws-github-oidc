341a580c237a55eb1165cf04a66459a804c7b082
Douglas Naphas <douglasnaphas@gmail.com>
2023-07-29 00:14:38 -0400

    Test more cases for oidcProviderArn2Url

--- tools/oidcProviderArn2Url.test.ts
+++ tools/oidcProviderArn2Url.test.ts
@@ -1,17 +1,38 @@
 import { oidcProviderArn2Url } from "./oidcProviderArn2Url";
 describe("oidcProviderArn2Url", () => {
-  test.each([
-    {
-      oidcProviderArn:
-        "arn:aws:iam::287213416300:oidc-provider/token.actions.githubusercontent.com",
-      expected: "token.actions.githubusercontent.com",
-    },
-    {
-      oidcProviderArn:
-        "arn:aws:iam::287213416300:oidc-provider/elsewhere.com",
-      expected: "elsewhere.com",
-    },
-  ])("$oidcProviderArn -> $expected", ({ oidcProviderArn, expected }) => {
-    expect(oidcProviderArn2Url(oidcProviderArn)).toEqual(expected);
+  describe("happy", () => {
+    test.each([
+      {
+        oidcProviderArn:
+          "arn:aws:iam::287213416300:oidc-provider/token.actions.githubusercontent.com",
+        expected: "token.actions.githubusercontent.com",
+      },
+      {
+        oidcProviderArn:
+          "arn:aws:iam::687000416300:oidc-provider/elsewhere.com",
+        expected: "elsewhere.com",
+      },
+      {
+        oidcProviderArn: "arn:aws:iam::444413416300:oidc-provider/",
+        expected: "",
+      },
+    ])("$oidcProviderArn -> $expected", ({ oidcProviderArn, expected }) => {
+      expect(oidcProviderArn2Url(oidcProviderArn)).toEqual(expected);
+    });
+  });
+  describe("error", () => {
+    test.each([
+      {
+        oidcProviderArn: "not-even-an-arn",
+      },
+      {
+        oidcProviderArn:
+          "arn:aws:iam::444413416300:more-subtly-wrong/oidc-provider",
+      },
+    ])("$oidcProviderArn", ({ oidcProviderArn }) => {
+      expect(() => {
+        oidcProviderArn2Url(oidcProviderArn);
+      }).toThrow();
+    });
   });
 });
