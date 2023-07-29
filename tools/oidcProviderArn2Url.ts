08c062fbf5264268aef7359b35d08549d9c04265
Douglas Naphas <douglasnaphas@gmail.com>
2023-07-29 00:06:23 -0400

    Implement oidcProviderArn2Url
    I need this so I can figure out if the account already has a GitHub OIDC
    Provider. There can only be one of those per AWS account.

--- tools/oidcProviderArn2Url.ts
+++ tools/oidcProviderArn2Url.ts
@@ -1,3 +1,7 @@
 export const oidcProviderArn2Url = (oidcProviderArn: string) => {
-  return "token.actions.githubusercontent.com";
+  const OIDC_PROVIDER_ARN_PATTERN = /arn:aws:iam::[0-9]{12}:oidc-provider\//;
+  if (!OIDC_PROVIDER_ARN_PATTERN.test(oidcProviderArn)) {
+    throw Error("invalid OIDC Provider ARN");
+  }
+  return oidcProviderArn.split(OIDC_PROVIDER_ARN_PATTERN)[1];
 };
