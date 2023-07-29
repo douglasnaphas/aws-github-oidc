57713dc524cf34405b117b55b3e64eb241ad84af
Douglas Naphas <douglasnaphas@gmail.com>
2023-07-28 23:30:37 -0400

    List the OIDC Providers for this account

--- tools/index.ts
+++ tools/index.ts
@@ -1,2 +1,14 @@
 #!/usr/bin/env npx ts-node
-console.log("running...");
+import {
+  IAMClient,
+  ListOpenIDConnectProvidersCommand,
+} from "@aws-sdk/client-iam";
+(async () => {
+  console.log("running...");
+  const region = process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION;
+  const client = new IAMClient({ region });
+  const input = {};
+  const command = new ListOpenIDConnectProvidersCommand(input);
+  const response = await client.send(command);
+  console.log(response);
+})();
