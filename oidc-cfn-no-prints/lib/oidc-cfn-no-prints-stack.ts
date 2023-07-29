48384e47768209f81e286b15ca121fc2086de30a
Douglas Naphas <douglasnaphas@gmail.com>
2023-07-29 09:23:11 -0400

    Init a CDK project in oidc-cfn-no-prints

---
+++ oidc-cfn-no-prints/lib/oidc-cfn-no-prints-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class OidcCfnNoPrintsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'OidcCfnNoPrintsQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
