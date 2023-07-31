9ac1c888f3efdec9846e7060214bc4626f029347
Douglas Naphas <douglasnaphas@gmail.com>
2023-07-31 11:05:20 -0400

    Execute from got1, fail with invalid credentials

---
+++ tools/got1.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env npx ts-node
+import {
+  IAMClient,
+  ListOpenIDConnectProvidersCommand,
+} from "@aws-sdk/client-iam";
+import { containsGitHubProvider } from "./containsGitHubProvider";
+(async () => {
+  const region = process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION;
+  const client = new IAMClient({ region });
+  const input = {};
+  const command = new ListOpenIDConnectProvidersCommand(input);
+  let response;
+  try {
+    response = await client.send(command);
+  } catch (error: any) {
+    if (error.Code === "ExpiredToken") {
+      console.error(
+        "expired token, try setting the variables AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and AWS_SESSION_TOKEN to valid credentials"
+      );
+      const ARBITRARY_NONZERO_STATUS = 2;
+      process.exit(ARBITRARY_NONZERO_STATUS);
+    }
+  }
+  console.log("response:\n", response);
+})();
