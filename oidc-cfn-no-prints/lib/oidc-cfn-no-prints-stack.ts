6edec32c66a2ab253ed94d6458f182624b925118
Douglas Naphas <douglasnaphas@gmail.com>
2023-08-03 17:12:51 -0400

    Add a GitHub provider
    Based on
    https://github.com/aripalo/aws-cdk-github-oidc/blob/53de62a59834455b89f9a567468233daf710e5b9/src/provider.ts#L51-L56.

--- oidc-cfn-no-prints/lib/oidc-cfn-no-prints-stack.ts
+++ oidc-cfn-no-prints/lib/oidc-cfn-no-prints-stack.ts
@@ -1,16 +1,18 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class OidcCfnNoPrintsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'OidcCfnNoPrintsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const gitHubOIDCProvider = new iam.OpenIdConnectProvider(
+      this,
+      "GitHubOIDCProvider",
+      {
+        url: "https://token.actions.githubusercontent.com",
+        clientIds: ["sts.amazonaws.com"],
+      }
+    );
   }
 }
