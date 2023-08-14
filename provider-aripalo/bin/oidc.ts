74d6fa55285667f29379226ff4fb0ab70253e098
Douglas Naphas <douglasnaphas@gmail.com>
2023-08-13 22:49:07 -0400

    Remove the aripalo option
    Now that I have a reasonable workflow using the L2 Construct for the
    OIDC provider, I don't see a justification for using the construct
    removed here.

--- provider-aripalo/bin/oidc.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
-import { OidcStack } from "../lib/oidc-stack";
-
-const app = new cdk.App();
-// figure out if there is already an OIDC provider in this account
-new OidcStack(app, "GitHubOIDCProvider");
--- provider-aripalo/test/oidc.test.ts
+++
@@ -1,8 +0,0 @@
-import * as cdk from "aws-cdk-lib";
-import { Template } from "aws-cdk-lib/assertions";
-import * as Oidc from "../lib/oidc-stack";
-
-test("Can instantiate", () => {
-  const app = new cdk.App();
-  const stack = new Oidc.OidcStack(app, "MyTestStack");
-});
