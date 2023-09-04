ce80e9e5fe155b2b4da3e0e9145ed9fc23125a5a
Douglas Naphas <douglasnaphas@gmail.com>
2023-09-04 08:08:37 -0400

    Fix provider import

--- GitHubOIDCProvider/bin/GitHubOIDCProvider.ts
+++ GitHubOIDCProvider/bin/GitHubOIDCProvider.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { GitHubOIDCProviderStack } from "../lib/oidc-cfn-no-prints-stack";
+import { GitHubOIDCProviderStack } from "../lib/GitHubOIDCProvider";
 
 const app = new cdk.App();
 new GitHubOIDCProviderStack(app, "GitHubOIDCProviderStack", {});
