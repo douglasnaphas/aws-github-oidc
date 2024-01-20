7db49f78400a8fa7dbd178c38043a0629ae3783e
Douglas Naphas <douglasnaphas@gmail.com>
2024-01-20 18:36:47 -0500

    Remove unnecessary helpers for using Providers
    We know the GitHub OIDC Provider ARN, so repos can use SDK calls
    directly to check if one exists.

--- oidcProviderArn2Url.ts
+++
@@ -1,7 +0,0 @@
-export const oidcProviderArn2Url = (oidcProviderArn: string) => {
-  const OIDC_PROVIDER_ARN_PATTERN = /arn:aws:iam::[0-9]{12}:oidc-provider\//;
-  if (!OIDC_PROVIDER_ARN_PATTERN.test(oidcProviderArn)) {
-    throw Error("invalid OIDC Provider ARN");
-  }
-  return oidcProviderArn.split(OIDC_PROVIDER_ARN_PATTERN)[1];
-};
