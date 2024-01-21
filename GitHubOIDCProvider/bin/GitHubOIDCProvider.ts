d0b3c7624b1664d1f4fc0ab7ef24e4f6c71c72a2
Douglas Naphas <douglasnaphas@gmail.com>
2024-01-21 09:26:16 -0500

    Focus on the Role package, not top-level
    The only part of this repo I'm currently envisioning my many users
    interacting with in a package sense is GitHubOIDCRoleStack/.

--- GitHubOIDCProvider/bin/GitHubOIDCProvider.ts
+++ GitHubOIDCProvider/bin/GitHubOIDCProvider.ts
@@ -3,5 +3,6 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { GitHubOIDCProviderStack } from "../lib/GitHubOIDCProvider";
 
+
 const app = new cdk.App();
 new GitHubOIDCProviderStack(app, "GitHubOIDCProviderStack", {});
