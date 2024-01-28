43e143127751fd2c8995ef047b1ffdc801940618
Douglas Naphas <douglasnaphas@gmail.com>
2024-01-28 09:20:22 -0500

    Use jq -r instead of piping to tr to strip quotes

--- GitHubOIDCRoleStack/pre-release.sh
+++ GitHubOIDCRoleStack/pre-release.sh
@@ -1,12 +1,12 @@
 echo "running pre-release"
-PACKAGE_JSON_VERSION=refs/tags/v$(cat package.json | jq '.version' | tr -d '"')
+PACKAGE_JSON_VERSION=refs/tags/v$(cat package.json | jq --raw-output '.version')
 if [[ "${PACKAGE_JSON_VERSION}" != "${GITHUB_REF}" ]]
 then
   echo -n "error: PACKAGE_JSON_VERSION ${PACKAGE_JSON_VERSION} mismatches "
   echo    "GITHUB_REF ${GITHUB_REF}"
   exit 1
 fi
-PACKAGE_LOCK_JSON_VERSION=refs/tags/v$(cat package-lock.json | jq '.version' | tr -d '"')
+PACKAGE_LOCK_JSON_VERSION=refs/tags/v$(cat package-lock.json | jq --raw-output '.version')
 if [[ "${PACKAGE_LOCK_JSON_VERSION}" != "${GITHUB_REF}" ]]
 then
   echo -n "error: PACKAGE_LOCK_JSON_VERSION ${PACKAGE_LOCK_JSON_VERSION} mismatches "
